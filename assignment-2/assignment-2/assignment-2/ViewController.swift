1a064d7fa997cc60ff3fd8e57d5d407a2fc5b7d2
Serena Bailey <47674941+BaileySerena@users.noreply.github.com>
2020-02-13 20:20:46 -0600

    Add files via upload
    Assignment-2

---
+++ assignment-2/assignment-2/assignment-2/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  assignment-2
+//
+//  Created by Serena Bailey on 2/13/20.
+//  Copyright © 2020 Serena Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
