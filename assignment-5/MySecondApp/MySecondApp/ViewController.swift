8af074e4db3c26d5335f862cc19177e37a046fbf
Serena Bailey <47674941+BaileySerena@users.noreply.github.com>
2020-03-11 13:57:54 -0500

    Add files via upload

---
+++ assignment-5/MySecondApp/MySecondApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by Serena Bailey on 3/11/20.
+//  Copyright © 2020 Serena Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
