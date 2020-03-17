88698aca8b1945fe41a28c56a713faa81a59b9da
Serena Bailey <47674941+BaileySerena@users.noreply.github.com>
2020-03-17 17:39:40 -0500

    Add files via upload

---
+++ assignment-6/ObjectApp/ObjectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectApp
+//
+//  Created by Serena Bailey on 3/17/20.
+//  Copyright © 2020 Serena Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
